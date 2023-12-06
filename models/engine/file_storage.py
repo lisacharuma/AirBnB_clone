5ec72a5bb445fc43edbddadd5486cee4363ba96b
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-12-06 11:00:27 +0000

    figuring out task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,36 @@
 #!/usr/bin/python3
+from os.path import exists
+import json
 
 class FileStorage:
-    ...
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        try:
+            if exists(self.__file_path):
+                with open(self.__file_path, 'r') as file:
+                    infomation = json.load(file)
+
+                    for key, obj in infomation.items():
+                        class_name, obj_id = key.split('.')
+                        obj_class = globals()[class_name]
+                        obj_instance = obj_class(**obj)
+                        self.__objects[key] = obj_instance
+        except FileNotFoundError:
+            pass
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 import unittest
+import uuid
+from datetime import datetime
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     def test_id_is_str(self):
         my_model = BaseModel()
         self.assertIsInstance(my_model.id, str)
+        
+    #def test_if_updated_at_method(self):
+        #""" test if the updated_at is working """
+        #my_model = BaseModel()
+        #original_updated_at = self.my_model.updated_at
+        #self.my_model.save()
+        #self.assertNotEqual(self.my_model.save, original_updated_at)
+
+    def test_for_uuid(self):
+        """ tests if uuid is unique """
+        my_model = BaseModel()
+        my_uuid = str(uuid.uuid4())
+        self.assertNotEqual(my_model.id, my_uuid)
+
+    def test_if_date_was_created(self):
+        """ test if date was created """
+        my_model = BaseModel()
+        time = datetime.utcnow()
+        self.assertEqual(my_model.created_at, time)
+
+    def test_if_date_is_ISO(self):
+        ...
+
 
 if __name__ == "__main__":
     unittest.main()
