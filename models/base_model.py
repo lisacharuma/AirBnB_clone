28ae84c4060f867895ddd8e3f984e58dd574e799
lisacharuma <charumalisah@gmail.com>
2023-12-08 01:18:00 -0800

    Fixed pycodestyle issues

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,13 @@
 import uuid
 from datetime import datetime
 import models
+"""BaseModel module"""
+
 
 class BaseModel:
+    """Reprsents BaseModel instances"""
     def __init__(self, *args, **kwargs):
+        """Initializes BaseModel"""
         class_name = None
         if kwargs:
             """class_name = kwargs.pop("__class__", None)"""
@@ -12,7 +16,8 @@ class BaseModel:
                 if key == "__class__":
                     pass
                 elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, time)
                 else:
                     setattr(self, key, value)
         else:
@@ -24,14 +29,17 @@ class BaseModel:
             setattr(self, "__class__", class_name)"""
 
     def __str__(self):
+        """Defines string rep for instances"""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
+        """Saves instances"""
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
+        """convert instances to dict"""
         object_dict = self.__dict__.copy()
         object_dict["__class__"] = self.__class__.__name__
         object_dict["created_at"] = self.created_at.isoformat()
